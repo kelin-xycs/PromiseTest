639890fc6c5c6d6192da60b1a555c52cba543613
kelin-xycs <kelin_xycs@163.com>
2019-01-07 22:56:10 +0800

    Signed-off-by: kelin-xycs <kelin_xycs@163.com>

---
+++ PromiseTest/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace PromiseTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            Promise.When((p) => { Read1(p); }, (p) => { Read2(p);  }).Then( (p) => { Read3(p); } );
+        }
+
+        // 如果 参数 p 传 null， 则作为 普通方法 调用
+        private void Read1(Promise p)
+        {
+            byte[] b = new byte[2048];
+
+            var s = File.Open("aa1.txt", FileMode.Open);
+
+            p.State1 = s;   //  可在这里设置要传给 ThenAction 的 state 对象
+
+            s.BeginRead(b, 0, 2048, CompletedRead1, p);
+        }
+
+        private void CompletedRead1(IAsyncResult ar)
+        {
+
+            WriteMsg("Read aa1.txt 完成");
+
+            Promise p = (Promise)ar.AsyncState;
+
+            if (p != null)
+            {
+                ((Stream)p.State1).Dispose();
+
+                p.SetCompleted1();
+            }
+        }
+
+        private void Read2(Promise p)
+        {
+            byte[] b = new byte[2048];
+
+            var s = File.Open("aa2.txt", FileMode.Open);
+
+            p.State2 = s;   //  可在这里设置要传给 ThenAction 的 state 对象
+
+            s.BeginRead(b, 0, 2048, CompletedRead2, p);
+        }
+
+        private void CompletedRead2(IAsyncResult ar)
+        {
+
+            WriteMsg("Read aa2.txt 完成");
+
+            Promise p = (Promise)ar.AsyncState;
+
+            if (p != null)
+            {
+                ((Stream)p.State2).Dispose();
+
+                p.SetCompleted2();
+            }
+        }
+
+        private void Read3(Promise p)
+        {
+            byte[] b = new byte[2048];
+
+            var s = File.Open("aa3.txt", FileMode.Open);
+
+            p.State3 = s;   //  可在这里设置要传给 ThenAction 的 state 对象
+
+            s.BeginRead(b, 0, 2048, CompletedRead3, p);
+        }
+
+        private void CompletedRead3(IAsyncResult ar)
+        {
+
+            WriteMsg("Read aa3.txt 完成");
+
+
+            Promise p = (Promise)ar.AsyncState;
+
+            if (p != null)
+            {
+                ((Stream)p.State3).Dispose();
+                // 可通过 p.State1 和 p.State2 获取 Action1 和 Action2 传过来的 State 对象
+            }
+        }
+
+        private void WriteMsg(string msg)
+        {
+            txtMsg.AppendText(DateTime.Now.ToString("HH:mm:ss.fff") + " " + msg + "\r\n");
+        }
+    }
+}
---
+++ PromiseTest/Promise.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromiseTest
+{
+    class Promise
+    {
+        private Action<Promise> a1;
+        private Action<Promise> a2;
+        private Action<Promise> a3;
+
+        private object obj = new object();
+
+        private bool a1Completed;
+        private bool a2Completed;
+        private bool allCompleted;
+
+        public object State1;
+        public object State2;
+        public object State3;
+
+        //private Para para = new Para();
+
+        private Promise(Action<Promise> a1, Action<Promise> a2)
+        {
+            this.a1 = a1;
+            this.a2 = a2;
+        }
+
+        public static Promise When(Action<Promise> a1, Action<Promise> a2)
+        {
+            var p = new Promise(a1, a2);
+
+            return p;
+        }
+
+        public void Then(Action<Promise> a3)
+        {
+            this.a3 = a3;
+
+            a1(this);
+            a2(this);
+        }
+
+        public void SetCompleted1()
+        {
+            this.a1Completed = true;
+
+            DoCompleted();
+        }
+
+        public void SetCompleted2()
+        {
+            this.a2Completed = true;
+
+            DoCompleted();
+        }
+
+        public void DoCompleted()
+        {   
+            lock (this.obj)
+            {
+                if (this.allCompleted)
+                {
+                    return;
+                }
+
+                if (this.a1Completed && this.a2Completed)
+                {
+                    allCompleted = true;
+                }
+            }
+
+            if (this.allCompleted)
+            {
+                this.a3(this);
+            }
+        }
+
+        //public class Para
+        //{
+        //    public object State1;
+        //    public object State2;
+        //}
+
+        //public delegate void ThenAction(Promise p);
+
+    }
+}
